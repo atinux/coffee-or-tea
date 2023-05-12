76081e0f6c31d19141f8abb8f9a0ce56a77f668a
Sébastien Chopin <seb@nuxt.com>
2023-05-12 13:32:33 +0100

    Revert "demo ready"
    This reverts commit 78141c31bc19726abe52b86843238cf361f73283.

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,5 @@
-<script setup>
-const { loggedIn, user, clear } = useUserSession()
-const { data: votes, refresh } = await useFetch('/api/votes')
-
-const userChoice = computed(() => votes.value.find(vote => vote.userId === user.value?.id)?.choice)
-const totalCoffee = computed(() => votes.value.filter(vote => vote.choice === 'coffee').length)
-const totalTea = computed(() => votes.value.filter(vote => vote.choice === 'tea').length)
-
-let interval
-onMounted(() => {
-  setInterval(refresh, 1000)
-})
-onBeforeUnmount(() => {
-  clearInterval(interval)
-})
-
-function vote(choice) {
-  if (!loggedIn.value) return alert('You need to login to vote')
-  $fetch('/api/vote', {
-    method: 'POST',
-    body: { choice }
-  }).then(refresh)
-}
-</script>
-
 <template>
   <div class="min-h-screen flex flex-col gap-8 items-center pt-4">
-    <h1 class="text-3xl">Coffee and Tea?</h1>
-    <div class="flex items-center gap-4">
-      <button @click="vote('coffee')" class="vote" :class="{ '!bg-gray-100': userChoice === 'coffee' }">☕ {{ totalCoffee }}</button>
-      <button @click="vote('tea')" class="vote" :class="{ '!bg-gray-100': userChoice === 'tea' }">🍵 {{ totalTea }}</button>
-    </div>
-    <div>
-      <p v-if="loggedIn">Welcome {{ user.login }}, <button @click="clear" class="underline">logout</button></p>
-      <a v-else href="/api/auth" class="bg-black text-white px-4 py-2 rounded">Login with GitHub to vote</a>
-    </div>
-    <ul>
-      <li v-for="vote in votes" :key="vote.id">
-        {{ vote.choice === 'coffee' ? '☕️' : '🍵' }} by {{ vote.username }}
-      </li>
-    </ul>
+    <h1 class="text-3xl">Vue London Demo</h1>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-button.vote {
-  @apply border rounded p-4 hover:bg-gray-50;
-}
-</style>
