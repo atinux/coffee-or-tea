2db77fe211fe5b0e442f9ab6970e5d316a9d4eaa
Sébastien Chopin <seb@nuxt.com>
2023-05-25 09:15:10 -0500

    reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: 'nuxt-space-layer',
+  // extends: 'nuxt-space-layer',
   modules: [
     '@nuxt/devtools',
     '@nuxtjs/tailwindcss'
